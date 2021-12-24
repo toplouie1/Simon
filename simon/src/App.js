2e4f3e86cb5b52fcc3fef6013ca700919ea65f63
Nima sherpa <Toplouie78@gmail.com>
2021-12-24 12:02:51 -0500

    css and the base functionailty

--- simon/src/App.js
+++ simon/src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-	return <div className="App"></div>;
+	const boxes = ["yellow", "blue", "red", "green"];
+	const [computerOrder, setComputerOrder] = useState([]);
+	const [playerOrder, setPlayerOrder] = useState([]);
+
+	const handleBlink = (event) => {
+		event.target.styple.opacity = 1;
+		settimeout(() => {
+			event.target.style.opacity = 0.5;
+		}, 333);
+	};
+
+	const handleClickBox = (boxIndex, e) => {
+		handleBlink(e);
+	};
+	let boxesElArr = boxes.map((color, index) => {
+		return (
+			<div
+				key={index}
+				onClick={(e) => handleClickBox(index, e)}
+				style={{ backgroundColor: color }}
+				className={"box"}
+			></div>
+		);
+	});
+
+	return <div className="App">{boxesElArr}</div>;
 }
 
 export default App;
