04663b184bde9b46dcb9b9856799a7b488970dff
Nima sherpa <Toplouie78@gmail.com>
2021-12-24 12:31:47 -0500

    added the method to add a random index in a array

--- simon/src/App.js
+++ simon/src/App.js
@@ -3,8 +3,16 @@ import { useState } from "react";
 
 function App() {
 	const boxes = ["yellow", "blue", "red", "green"];
-	const [computerOrder, setComputerOrder] = useState([]);
-	const [playerOrder, setPlayerOrder] = useState([]);
+	const [computerOrder, setComputerOrder] = useState([3]);
+	// const [playerOrder, setPlayerOrder] = useState([]);
+
+	const handleAddRandomBox = () => {
+		// generating random index from 0 to 3
+		let randomIndex = Math.floor(Math.random() * 4);
+		// creating a new array with the added random numbers
+		// then setting them to the computerOrder array .
+		setComputerOrder([...computerOrder, randomIndex]);
+	};
 
 	const handleBlink = (event) => {
 		event.target.styple.opacity = 1;
@@ -15,7 +23,9 @@ function App() {
 
 	const handleClickBox = (boxIndex, e) => {
 		handleBlink(e);
+		handleAddRandomBox();
 	};
+
 	let boxesElArr = boxes.map((color, index) => {
 		return (
 			<div
