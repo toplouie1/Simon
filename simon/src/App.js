4abc4ce2150e16b3e1f0c5ebad71615056936bdb
Nima sherpa <Toplouie78@gmail.com>
2021-12-25 15:28:09 -0500

    stroing the index and checking if it is correct

--- simon/src/App.js
+++ simon/src/App.js
@@ -3,9 +3,10 @@ import { useState, useEffect } from "react";
 
 function App() {
 	const boxes = ["yellow", "blue", "red", "green"];
-	const [computerOrder, setComputerOrder] = useState([3, 0, 1, 2]);
-	// const [playerOrder, setPlayerOrder] = useState([]);
+	const [computerOrder, setComputerOrder] = useState([3]);
+	const [playerOrder, setPlayerOrder] = useState([]);
 	const [selectedBox, setSelectedBox] = useState(null);
+	const [isGameOver, setIsGameOver] = useState(false);
 
 	const handleAddRandomBox = () => {
 		// generating random index from 0 to 3
@@ -35,24 +36,41 @@ function App() {
 
 	const handleClickBox = (boxIndex) => {
 		handleBlink(boxIndex);
+		// giving it a condition
+		let isCorrectOrder = true;
+		for (let i = 0; i < playerOrder.length; i++) {
+			if (playerOrder[i] !== computerOrder[i]) {
+				isCorrectOrder = false;
+			}
+		}
+		if (!isCorrectOrder) {
+			setIsGameOver(true);
+			return;
+		}
+
+		// setPlayerOrder([...playerOrder, boxIndex]);
 	};
 
 	let boxesElArr = boxes.map((color, index) => {
 		return (
 			<div
 				key={index}
-				onClick={(e) => handleClickBox(index, e)}
+				onClick={() => handleClickBox(index)}
 				style={{
 					backgroundColor: color,
 					opacity: selectedBox === index ? 1 : 0.5,
 				}}
-				id={"box-" + index}
 				className={"box"}
 			></div>
 		);
 	});
 
-	return <div id="boxes-container">{boxesElArr}</div>;
+	return (
+		<div id="boxes-container">
+			{!isGameOver && boxesElArr}
+			{isGameOver && <h1> Game is over You LOSE !!!!!!!</h1>}
+		</div>
+	);
 }
 
 export default App;
